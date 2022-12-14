0f502c89a550064984933e3f9e82b438c9de051c
Syed Atyab Hussain <syed.atyab@codeline.rihal.om>
2022-12-13 21:24:43 -0800

    Added new project files

---
+++ src/FirstUtility.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author SyedAtyabHussain
+ *
+ */
+public class FirstUtility {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hey, this is working...");
+
+    }
+
+}
